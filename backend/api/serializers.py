7c7eb650629efc17476ca73ecf51077f9e2d912f
Артур Степанов <extrimiti@yandex.ru>
2023-08-07 14:44:56 +0300

    PEP8

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -30,7 +30,11 @@ class CustomUserSerializer(UserSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "email", "username", "first_name", "last_name", "is_subscribed"]
+        fields = [
+            "id", "email",
+            "username", "first_name",
+            "last_name", "is_subscribed"
+        ]
 
     def get_is_subscribed(self, obj):
         request = self.context.get("request")
